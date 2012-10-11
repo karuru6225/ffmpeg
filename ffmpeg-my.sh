517761734eac9a1645131b53ed1fa085c6bfd118
kano <kano@nextconceptdc.com>
2012-10-11 20:17:53 +0900

    presetの設定変更。エンコスクリプトに引数チェックを追加。

--- ffmpeg-my.sh
+++ ffmpeg-my.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ffmpeg -y -i "$1" -vcodec libx264 -s 1280x720 -r 30000/1001 -g 30 -vprofile high -acodec libfaac -ar 48000 -ab 192k -threads 4 -fpre ~/.ffmpeg/my-preset.ffpreset "$2"
+if [ $# != 2 ]; then
+	echo "$0 [srcfile] [dstfile]"
+else
+	ffmpeg -y -i "$1" -vcodec libx264 -s 1280x720 -r 30000/1001 -g 30 -vprofile high -acodec libfaac -ar 48000 -ab 192k -threads 4 -fpre ~/.ffmpeg/my-preset.ffpreset "$2"
+fi
